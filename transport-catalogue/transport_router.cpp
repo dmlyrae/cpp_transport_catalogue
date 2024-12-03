56b98293bc97bea7ac38c31e5966079d84082259
dmlyrae <dm.lyrae@ya.ru>
2024-12-03 22:36:39 +0500

    Убрал лишние операции

--- transport-catalogue/transport_router.cpp
+++ transport-catalogue/transport_router.cpp
@@ -22,53 +22,57 @@ const graph::DirectedWeightedGraph<double>& RouterA::BuildGraph(const Transport:
 		++vertex_id;
 	}
 	stop_ids_ = std::move(stop_ids);
-
 	for_each(
 		all_buses.begin(),
 		all_buses.end(),
 		[&stops_graph, this, &catalogue](const auto& item) {
-			// item - это std::pair<const std::string_view, std::shared_ptr<Bus>>
-			// const std::string_view& key = item.first; // Ключ
 			const std::shared_ptr<Bus>& busPtr = item.second; // Указатель на Bus
-			auto routeIt = busPtr->begin();
-			std::vector<std::shared_ptr<Stop>> stops;
-			while (routeIt != busPtr->end()) {
-				stops.push_back(routeIt->stop);
-				routeIt = routeIt->next;
-			}
-			stops.push_back(routeIt->stop);
-			size_t stops_count = stops.size();
-			for (size_t i = 0; i < stops_count; ++i) {
-				for (size_t j = i + 1; j < stops_count; ++j) {
-					const std::shared_ptr<Stop> stop_from = stops[i];
-					const std::shared_ptr<Stop> stop_to = stops[j];
+			auto firstRouteIt = busPtr->begin();
+			while (firstRouteIt) {
+				auto secondRouteIt = firstRouteIt->next;
+				while (secondRouteIt) {
+					const std::shared_ptr<Stop> stop_from = firstRouteIt->stop;
+					const std::shared_ptr<Stop> stop_to = secondRouteIt->stop;
+
 					int dist_sum = 0;
 					int dist_sum_inverse = 0;
-					for (size_t k = i + 1; k <= j; ++k) {
-						dist_sum += catalogue.GetDistance(stops[k - 1], stops[k]);
-						dist_sum_inverse += catalogue.GetDistance(stops[k], stops[k - 1]);
+
+					// Для подсчета расстояний между остановками
+					auto currentIt = firstRouteIt->next; // Начинаем с первой остановки после stop_from
+					auto prevIt = firstRouteIt; 
+					size_t distance_between_stops = 1;
+					while (currentIt != secondRouteIt->next) {
+						dist_sum += catalogue.GetDistance(prevIt->stop, currentIt->stop);
+						dist_sum_inverse += catalogue.GetDistance(currentIt->stop, prevIt->stop);
+						prevIt = currentIt;
+						currentIt = currentIt->next;
+						++distance_between_stops;
 					}
+
+					// Добавляем ребро в граф
 					stops_graph.AddEdge({ 
-							busPtr->GetName(),
-							j - i,
-							stop_ids_.at(stop_from->GetName()) + 1,
-							stop_ids_.at(stop_to->GetName()),
-							static_cast<double>(dist_sum) / (bus_velocity_ * (100.0 / 6.0))
-						});
+						busPtr->GetName(),
+						distance_between_stops, 
+						stop_ids_.at(stop_from->GetName()) + 1,
+						stop_ids_.at(stop_to->GetName()),
+						static_cast<double>(dist_sum) / (bus_velocity_ * (100.0 / 6.0))
+					});
 
 					if (busPtr->IsLine()) {
 						stops_graph.AddEdge({ 
 							busPtr->GetName(),
-							j - i,
+							distance_between_stops, 
 							stop_ids_.at(stop_to->GetName()) + 1,
 							stop_ids_.at(stop_from->GetName()),
 							static_cast<double>(dist_sum_inverse) / (bus_velocity_ * (100.0 / 6.0))
 						});
 					}
+
+					secondRouteIt = secondRouteIt->next;
 				}
+				firstRouteIt = firstRouteIt->next;
 			}
 		});
-
 	graph_ = std::move(stops_graph);
 	router_ = std::make_unique<graph::Router<double>>(graph_);
 
